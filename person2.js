76c3d8412525a1ef8074a323cbd1b0d50ff64a23
vijay.kumar <vijay.kumar@icreon.com>
2019-03-08 22:02:37 +0530

    updated files

--- person2.js
+++ person2.js
@@ -1,4 +1,3 @@
-/*****************************/
 db.person.aggregate([
     {
         $project: {
@@ -9,6 +8,22 @@ db.person.aggregate([
                 $concat: [
                     {$toUpper: {$substrCP: ["$gender", 0, 1]}},
                     {$substrCP: ["$gender", 1, {$subtract: [{$strLenCP: "$gender"}, 1]}]}
+                ]
+            },
+            title: {
+                $concat: [
+                    {$toUpper: {$substrCP: ["$name.title", 0, 1]}},
+                    {$substrCP: ["$name.title", 1, {$subtract: [{$strLenCP: "$name.title"}, 1]}]},
+                    "."
+                ]
+            },
+            fullName: {
+                $concat: [
+                    {$toUpper: {$substrCP: ["$name.first", 0, 1]}},
+                    {$substrCP: ["$name.first", 1, {$subtract: [{$strLenCP: "$name.first"}, 1]}]},
+                    ' ',
+                    {$toUpper: {$substrCP: ["$name.last", 0, 1]}},
+                    {$substrCP: ["$name.last", 1, {$subtract: [{$strLenCP: "$name.last"}, 1]}]}
                 ]
             }
         }
